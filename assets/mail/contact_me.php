12af3f231430f6c0816b5d92595a6e0add2e4a6d
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-05-28 14:34:30 +0300

    major fixes

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,26 +1,28 @@
 <?php
 
-if(empty($_POST['name'])      ||
+if (
+   empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+   empty($_POST['order'])          ||
+   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
    echo "Invalid";
    return false;
-   }
-   
+}
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+$orderType = strip_tags(htmlspecialchars($_POST['order']));
 
-$to = 'test@gmail.com'; 
+
+$to = 'test@gmail.com';
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@gmail.com\n"; 
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
+$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message\n\norderType: $orderType";
+$headers = "From: noreply@gmail.com\n";
+$headers .= "Reply-To: $email_address";
+mail($to, $email_subject, $email_body, $headers);
 return true;
-?>
